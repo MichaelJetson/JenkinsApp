1334538aab76d8b758b38f837b76452b58b1c82b
MichaelJetson <michaeljet1402@gmail.com>
2022-07-15 14:31:08 +0530

    Commited after adding Readline

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -8,7 +8,7 @@ namespace JenkinsApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins");
-            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
