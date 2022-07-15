ba45738960dbe685057d2ac620c620d0be929707
MichaelJetson <michaeljet1402@gmail.com>
2022-07-15 15:44:16 +0530

    committing after assignment

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-
+using New;
 namespace JenkinsApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Welcome to Jenkins");
-            Console.ReadLine();
+            Console.WriteLine("Enter the value of a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            MyMaths math = new MyMaths();
+            Console.WriteLine("Addition = " + math.add(a, b));
+            Console.WriteLine("Multiplication = " + math.add(a, b));
         }
     }
 }
---
+++ MyMaths/MyMaths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace New
+{
+    public class MyMaths
+    {
+        public int add(int a, int b)
+        {
+            return a+b;
+        }
+        
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
